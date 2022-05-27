15500ca75bf3130260a12c51c1fe5685e46b07b2
prathusa <35161910+prathusa@users.noreply.github.com>
2022-05-27 09:38:05 -0500

    update style of parser

--- DataWrangler/parser.py
+++ DataWrangler/parser.py
@@ -1,15 +1,10 @@
 import tabula, pandas as pd, csv
 from colorama import Fore, Style
 
+df = tabula.read_pdf("test.pdf", pages='all', area=[96.525, 71.28, 524.205, 716.76])
+
 def parse(): 
-    df = tabula.read_pdf("test.pdf", pages='1,2', area=[96.525, 71.28, 524.205, 716.76])
-    # print(df)
-    # df[0].to_csv("test.csv", index=True)
-    return df[0].to_csv()
-    # pages = []
-    # for page in df:
-    #     pages.append(page.to_csv())
-    # return pages
+    for page in df: yield page.to_csv()
 
 # def save(): 
     # print(f'{Fore.LIGHTBLUE_EX}[+]{Style.RESET_ALL} Saving to CSV...')
