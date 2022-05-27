6156d2532c7f114540ae3f8300f8b95c00bc9bfc
prathusa <35161910+prathusa@users.noreply.github.com>
2022-05-27 13:39:43 -0500

    Fixed subject function bug

--- DataWrangler/parser.py
+++ DataWrangler/parser.py
@@ -7,18 +7,19 @@ def load(filename, pages = '1'):
     print(f'{Fore.LIGHTCYAN_EX}Loaded {filename}...{Style.RESET_ALL}')
     return df
 
-def generator(filename, pages='1,2,3'):
-    for page in load(filename,pages): yield page.to_csv()
+df = load('test.pdf', '1,2,3')
+
+def generator():
+    for page in df: yield page.to_csv()
 
 def subject(page):
     string = page.splitlines()[1].split(",")[1]
-    return string[string.find(' ')+1:string.rfind(' ')]
+    return string[4:].replace(' Grades', '')
 
 # def save(): 
     # print(f'{Fore.LIGHTBLUE_EX}[+]{Style.RESET_ALL} Saving to CSV...')
     # for table in parse():
 
 if __name__ == '__main__':
-    page = generator('test.pdf')
-    print(subject(page.__next__()))
-    # print(page.__next__())
+    for page in generator():
+        print(subject(page))
