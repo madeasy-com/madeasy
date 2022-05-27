ed4449f09868da05502c8a770ffca450639dbcdb
prathusa <35161910+prathusa@users.noreply.github.com>
2022-05-27 09:35:48 -0500

    registrar pdf parser complete
    PDFs are being parsed correctly now
    What is left is to extract useful data from the parsed data in the format of json

--- DataWrangler/parser.py
+++ DataWrangler/parser.py
@@ -1,11 +1,24 @@
-from tika import parser
-import re
+import tabula, pandas as pd, csv
+from colorama import Fore, Style
 
-def parse():
-    parsed = parser.from_file('test.pdf')
-    content = parsed['content']
-    term_code = re.findall(r'TERM : .*', content)
-    re.findall(r'A AB B BC C D F S U CR N P I NW NR Other', content)
+def parse(): 
+    df = tabula.read_pdf("test.pdf", pages='1,2', area=[96.525, 71.28, 524.205, 716.76])
+    # print(df)
+    # df[0].to_csv("test.csv", index=True)
+    return df[0].to_csv()
+    # pages = []
+    # for page in df:
+    #     pages.append(page.to_csv())
+    # return pages
+
+# def save(): 
+    # print(f'{Fore.LIGHTBLUE_EX}[+]{Style.RESET_ALL} Saving to CSV...')
+    # for table in parse():
         
+
+def subject(page):
+    string = parse().splitlines()[1].split(",")[1]
+    return string[string.find(' ')+1:string.rfind(' ')]
+
 if __name__ == '__main__':
-    parse()
+    print(subject())
