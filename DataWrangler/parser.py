6c68c65935d41f7490d0519126719cb44377a270
prathusa <35161910+prathusa@users.noreply.github.com>
2022-05-28 23:00:18 -0500

    Made parser OOP

--- DataWrangler/parser.py
+++ DataWrangler/parser.py
@@ -1,27 +1,36 @@
 import tabula, pandas as pd, csv
 from colorama import Fore, Style
 
-def load(filename, pages = '1'):
-    print(f'{Fore.LIGHTCYAN_EX}Loading {filename}...{Style.RESET_ALL}')
-    df = tabula.read_pdf(filename, pages=pages, area=[96.525, 71.28, 524.205, 716.76])
-    print(f'{Fore.LIGHTCYAN_EX}Loaded {filename}...{Style.RESET_ALL}')
-    return df
+class Parser:
+    def __init__(self, filename, pages = 'all'):
+        print(f'{Fore.LIGHTCYAN_EX}Loading {filename}...{Style.RESET_ALL}')
+        self.data = tabula.read_pdf(filename, pages=pages, area=[96.525, 71.28, 524.205, 716.76])
+        print(f'{Fore.LIGHTCYAN_EX}Loaded {filename}...{Style.RESET_ALL}')
 
-def generator(filename, pages = '1,2,3'):
-    return [ page.to_csv() for page in load(filename, pages) ]
+    def __iter__(self):
+        for page in self.data: yield page
 
-def subject(page, pages):
-    string = page.splitlines()[1].split(",")[1]
-    if (department := string[4:].replace(' Grades', '')) != 'ary by College/School':
-        return department
-    else:
-        pages.remove(page)
+    def subject(self, page):
+        # return self.data
+        
+        # if 'Summary by College/School' == (string := page.splitlines()[1].split(",")[1]): self.data.remove(page)
+        # return string[4:].replace(' Grades', '')
     
-# def save(): 
-    # print(f'{Fore.LIGHTBLUE_EX}[+]{Style.RESET_ALL} Saving to CSV...')
-    # for table in parse():
+    def filter(self):
+        for page in self.data: 
+            self.subject(page)
+    
+    def save(self, filename): 
+        print(f'{Fore.LIGHTBLUE_EX}[+]{Style.RESET_ALL} Saving to CSV...')
+        with open(f'{filename}.pra', 'w') as file:
+            for page in self.data: file.write(str(page).strip())
+        print(f'{Fore.LIGHTBLUE_EX}[+]{Style.RESET_ALL} Saved to CSV!')
+            
 
 if __name__ == '__main__':
-    pages = generator('test.pdf', 'all')
-    for page in pages:
-        print(subject(page, pages))
+    pdf = Parser('test.pdf', '1,2,3')
+    # pdf.filter()
+    # pdf.save('test')
+    for page in pdf:
+        # print((page))
+        print(pdf.subject(page))
