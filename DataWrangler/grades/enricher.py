117f1521186bdcec7d44c77943d7d29349573c67
prathusa <prathusaf@gmail.com>
2023-01-23 07:08:12 -0600

    Need to merge uncross listed course data

--- DataWrangler/grades/enricher.py
+++ DataWrangler/grades/enricher.py
@@ -1,31 +1,41 @@
 import json, re
-from instructor import Instructor
+from os import listdir
+from collections import defaultdict
 
-def load(filename):
-    with open(filename, 'r') as file:
-        return json.load(file)
+# def load(filename):
+#     with open(filename, 'r') as file:
+#         return json.load(file)
+
+source = '../data/extracted/'
+ext = '.json'
 
 def enrich(filename):
-    raw = load(filename)
-    data = {}
-    term = re.findall(r'\d{4}', filename)[0]
-    for course in raw:
-        for section in raw[course]:
-            data.update({
-                course: {
-                    'distribution': raw[course][section]['distribution'],
-                    'instructors': instructors(course=course, term=term)[section],
-                }
-            })
-        # for section in course:
-            # section.update
-        # print(course)
-    # save(filename, data)
-    print(data)
+    files = listdir(source)
+    terms = []
+    # check if both dir and grade report exist for each term and only add one term to the list
+    [ terms.append(term[:-len(ext)]) for file in files if (term := file) and ext in term and term[:-len(ext)].isnumeric() ]
+
+    data = defaultdict(dict)
+
+    for term in terms:
+        with open(source+term+ext, 'r') as f:
+            d = json.load(f)
+            for k, v in d.items():
+                # print(k,v)
+                data[k][term] = v
+                # print(data) 
+                # break
+            # print(list(d.values())[0])
+    
+    save(source+filename+ext, data)
+    # print(terms)
+    # res = {}
+    # res.update()
+    
 
 def save(filename, file):
     with open(filename, 'w') as f:
         json.dump(file, f)
         
 if __name__ == "__main__":
-    enrich('extracted/1222.json')
+    enrich('all')
